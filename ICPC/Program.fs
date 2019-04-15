69cdee661080b658b90bbd8d3a44b9f7e84b3ada
Scott-Burnett <thebreakfastfactoryyay@gmail.com>
2019-04-15 16:22:50 +0200

    State machine experiment. Compile errors

--- ICPC/Program.fs
+++ ICPC/Program.fs
@@ -10,6 +10,39 @@ let stringToList (input:string) =
                     |s -> inner (i+1) (newWord + s) newList
     inner 1 "" []
 
+let getCommaBeforeWords (input:List<string>) =
+    let rec inner state (input:List<string>) (output:list<string>) =
+        match state with
+        |0 -> match input with
+            |(" "::t)|(","::t)|("."::t)|[] -> failwith "incorrect string format"
+            |s::t -> inner 1 t
+        |1 -> match input with
+            |","::t -> inner 2 t output
+            |"."::t -> inner 5 t output
+            |" "::t -> inner 6 t output
+            |_ -> failwith "incorrect string format" 
+        |2 -> match input with 
+            |" "::t -> inner 3 t output
+            |_ -> failwith "incorrect string format"
+        |3 -> match input with  
+            |","::t|" "::t|"."::t|[] -> failwith "incorrect string format"
+            |s::t -> inner 4 t (s @ output)
+        |4 -> match input with
+            |","::t -> inner 2 t output
+            |"."::t -> inner 5 t output
+            |" "::t -> inner 6 t output
+            |_ -> failwith "incorrect string format"
+        |5 -> match input with 
+            |" "::t -> inner 6 t output
+            |[] -> inner 7 input output
+            |_ -> failwith "incorrect string format" 
+        |6 -> match input with 
+            |" "::t|","::t|"."::t|[] -> failwith "incorrect string format"
+            |s::t -> inner 1 t output
+        |7 -> output
+    inner 0 input []
+
+
 let commaSprinkler input =
     failwith "Not implemented"
 
